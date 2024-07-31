a0c59635f4c486e1f63287fea05054ef6b02826e
AnnKiz3110 <120191753+AnnKiz3110@users.noreply.github.com>
2024-07-31 20:01:49 +0700

    first commit

---
+++ QLSB/ConnectionStringManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QLSB
+{
+    public class ConnectionStringManager
+    {
+        public static string GetConnectionString()
+        {
+            // Thay thế dòng sau bằng chuỗi kết nối thực tế của bạn
+            string connectionString = "Data Source=TRANAN3110\\SQLEXPRESS;Initial Catalog=QL_SANBONG_TEST1;User ID=sa;Password=123";
+            return connectionString;
+        }
+    }
+}
